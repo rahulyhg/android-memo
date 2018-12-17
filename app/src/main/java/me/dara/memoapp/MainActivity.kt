9a8c7691cfee96d0a96c907955788efd5fcfdb0b
= <sardor.islomov.96@gmail.com>
2018-12-17 22:51:25 +0500

    Firebase connected

---
+++ app/src/main/java/me/dara/memoapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.dara.memoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
